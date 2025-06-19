03a5c0f9d902ddc99c649509d4e9c41b51f15f60
Daniil138 <futurino120bom@gmail.com>
2025-06-20 00:25:00 +0800

    Closes #3; The code has been refactored and a setup file has been added to install the library.

---
+++ ionogram_visualizer/__init__.py
@@ -0,0 +1,5 @@
+from .visualizer import IonogramVisualizer
+from .BaseIonogramArrayBuilder import  BaseIonogramArrayBuilder
+from .SimpleIonogramArrayBuilder import SimpleIonogramArrayBuilder
+
+__all__ = ['IonogramVisualizer', 'SimpleIonogramArrayBuilder', 'BaseIonogramArrayBuilder']
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ionogram_visualizer",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.21.0',
+        'matplotlib>=3.5.0',
+        'ionread_python'  
+    ],
+    author="Daniil",
+    author_email="reetu",
+    description="Library for ionogram visualization and processing",
+    url="https://github.com/Daniil138/ionogram_show.git",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={
+        'ionogram_visualizer': '.',
+    },
+    python_requires='>=3.7',
+)
