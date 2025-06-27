ab5132e6cab429ce7b96312e19aae8fb0367d55c
Daniil138 <futurino120bom@gmail.com>
2025-06-27 14:47:17 +0800

    Add scale param, correct reaadme, correct add image

--- validation/compare.py
+++ validation/compare.py
@@ -1,65 +1,43 @@
-from ionogram_visualizer import  SimpleIonogramArrayBuilder
+from ionogram_visualizer import SimpleIonogramArrayBuilder
 import ionread_python as ionread
-
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.patches import Rectangle
 
-# Загрузка данных
 ionogram = ionread.read_ionogram('../test_data/01_02_07_20_00.dat')
-
-# Создание массива ионограммы
 builder = SimpleIonogramArrayBuilder(ionogram).process()
-ionogram_array = builder.get_ndarray()
-print("Размер исходного массива",ionogram_array.shape)
-#Сохранение
-plt.imsave(arr = ionogram_array, origin='lower', cmap='jet', fname="original_array.png")
+ion_arr = builder.get_ndarray()
 
 
+xtl, ytl = 70.44, 508-299.30
+xbr, ybr = 154.27, 508-336.18
 
-# Загрузка изображений
-img1 = plt.imread("result1.png")  # эталонное изображение
-img2 = plt.imread("original_array.png")   # сгенерированное изображение
 
-# Проверка размеров
-print("Размер result1.png:", img1.shape)
-print("Размер output.png:", img2.shape)
+dpi = 100  
+height, width = ion_arr.shape
+fig_width = width / dpi
+fig_height = height / dpi
 
-# Выбор области для сравнения (примерные координаты)
-x, y = 100, 100  # начальная точка
-width, height = 200, 200  # размер области
+fig, ax = plt.subplots(figsize=(fig_width, fig_height), dpi=dpi)
 
-# Проверка, чтобы область не выходила за границы изображений
-h1, w1 = img1.shape[0], img1.shape[1]
-h2, w2 = img2.shape[0], img2.shape[1]
-x = min(x, w1 - width, w2 - width)
-y = min(y, h1 - height, h2 - height)
+im = ax.imshow(ion_arr, cmap='jet', origin='lower', aspect='auto')
 
-# Создание фигуры с 4 подграфиками
-fig, axes = plt.subplots(2, 2, figsize=(12, 10))
 
-# Отображение полных изображений с выделенными областями
-axes[0, 0].imshow(img1)
-axes[0, 0].add_patch(Rectangle((x, y), width, height, linewidth=2, edgecolor='r', facecolor='none'))
-axes[0, 0].set_title('result1.png (оригинал)')
+rect = Rectangle((xtl, ytl),
+                xbr - xtl, 
+                ybr - ytl, 
+                linewidth=2,
+                edgecolor='r',
+                facecolor='none')
+ax.add_patch(rect)
 
-axes[0, 1].imshow(img2)
-axes[0, 1].add_patch(Rectangle((x, y), width, height, linewidth=2, edgecolor='r', facecolor='none'))
-axes[0, 1].set_title('output.png (сгенерированное)')
 
-# Отображение увеличенных областей
-zoom_img1 = img1[y:y+height, x:x+width]
-axes[1, 0].imshow(zoom_img1)
-axes[1, 0].set_title(f'Увеличенная область (result1.png)\n[{x}:{x+width}, {y}:{y+height}]')
+plt.axis('off')
+plt.subplots_adjust(left=0, right=1, top=1, bottom=0)  # убираем все отступы
 
-zoom_img2 = img2[y:y+height, x:x+width]
-axes[1, 1].imshow(zoom_img2)
-axes[1, 1].set_title(f'Увеличенная область (output.png)\n[{x}:{x+width}, {y}:{y+height}]')
 
-plt.tight_layout()
-
-# Сохранение в файл
-plt.savefig("compare.png", dpi=300, bbox_inches='tight')
-print("Сравнение сохранено в compare.png")
-
-# plt.show()  # Раскомментируйте, если нужно также показать в интерфейсе
+plt.savefig('ionogram_with_bbox.png', 
+           bbox_inches='tight',
+           pad_inches=0,
+           dpi=dpi)
+plt.close()
